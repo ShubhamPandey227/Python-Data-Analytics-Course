2f4a83214d257b2b66c4280de3fa3a64ae3d7aa6
Shubham Pandey <pandey.repu111@gmail.com>
2023-01-30 11:33:37 +0530

    first commit

---
+++ si.py
@@ -0,0 +1,10 @@
+# SI Calculator
+p = 10000
+r = 0.15
+t = 10
+s = p * r * t /100
+print(f'Principal is ${p}')
+print(f'Rate is {r}%')
+print(f'TIme is {t} yrs')
+print('---'*10)
+print(f"Simple Interest is: {s}")
