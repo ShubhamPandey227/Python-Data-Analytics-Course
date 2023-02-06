7769fa3cf8c69c0c1af08cbd8f16d0978c9a3d4a
Shubham Pandey <pandey.repu111@gmail.com>
2023-02-06 12:35:58 +0530

    Conditions

---
+++ if_else.py
@@ -0,0 +1,5 @@
+x = input("Enter a number : ")
+if x.isnumeric() :
+    print ("Very good")
+else :
+    print ("You're stupid 🤨 ")
---
+++ odd_even.py
@@ -0,0 +1,5 @@
+x = int(input("Enter the number to be checked : "))
+if (x % 2 == 0) :
+    print ("The No. is EVEN")
+else : 
+    print ("Odd Number")
